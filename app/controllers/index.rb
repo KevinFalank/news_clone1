e7ce5e7c768f059362ac90060c27404b4b113e2b
Apprentice <apprentice@dbc-chicago-021.local>
2014-02-05 14:29:58 -0600

    Created an add post view. Added get route for creating a post.
    Created add_post.erb and a get route called /posts/new  This takes us
    to our create route form.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,15 @@ get '/' do
   erb :index
 end
 
+get '/posts/new' do
+  erb :new_post
+end
+
+# post '/posts/new' do
+#   @post = params[:post]
+
+# end
+
 get '/posts/:post_id' do
 
   @post = Post.find(params[:post_id])
