5e7982c357c0e5335fec4f73fcaeaeb5639479a1
Apprentice <apprentice@dbc-chicago-021.local>
2014-02-05 16:54:58 -0600

    Added sign-in page
    Added a sign-in view called sign_in.erb
    Added a Post route to user to accept the sign_in information
    Only permitted sign-in if the password matched the user password in
    the database
    
    Don't remember why we modified comments, and don't know how to
    check what we modified.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,21 @@
+get '/users/signin' do
+  erb :sign_in
+end
+
 get '/users/new' do
   erb :new_user
+end
+
+post '/users/signin' do
+  @email = params[:email]
+  @password = params[:password]
+  user = User.where("email = ?", @email)[0]
+  if user.password == @password
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/users/signin'
+  end
 end
 
 post '/users/new' do
@@ -12,7 +28,8 @@ end
 
 
 get '/users/:user_id/comments' do
-
+  @comments = Comment.where("user_id = ?", params[:user_id])
+  erb :comments
 end
 
 get '/users/:user_id/posts' do
