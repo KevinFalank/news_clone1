d89dd14d1a1ca7aee3f80dcff10e8052e3efeb66
Apprentice <apprentice@dbc-chicago-021.local>
2014-02-05 14:12:08 -0600

    Added post view and added comment counts to index view
    Created post.erb which is our view for post.  Created it according
    to the wireframe in README_imgs.
    
    In index.erb created /posts/:id route, which displays a post and
    a comment. Comments were displaying in an odd order, so we learned
    how to use the sort_by to arrange them by date.
    http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-sort_by
    
    Also added comment count to index.erb.  Go us!

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,12 @@ get '/' do
   @posts = Post.all
   erb :index
 end
+
+get '/posts/:post_id' do
+
+  @post = Post.find(params[:post_id])
+  @comments = @post.comments.sort_by { |comment|  comment.created_at}
+  @user = @post.user
+  erb :post
+
+end
